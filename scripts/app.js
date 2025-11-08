1de2f75dda90ad50718a222a9df4fd910d655b40
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-11-08 14:21:45 +0200

    Update script

--- scripts/app.js
+++ scripts/app.js
@@ -8,9 +8,11 @@ if (burgerMenu) {
     burgerMenu.classList.toggle("active");
 
     if (burgerMenu.classList.contains("active")) {
+      lenis.stop();
       menuBackground.classList.add("active");
       mainMenu.classList.add("active");
     } else {
+      lenis.start();
       menuBackground.classList.remove("active");
       mainMenu.classList.remove("active");
     }
@@ -49,3 +51,12 @@ accordionCol.forEach((col) => {
     });
   });
 });
+
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
